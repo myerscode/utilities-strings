f27f9b90aee3d291a70d3f75a9d5171858456cf5
Fred Myerscough <oniice@gmail.com>
2017-09-04 18:06:01 +0100

    test: corrected test that had a space removed, and added an extra test to ensure spaces are preserved

--- tests/CleanTest.php
+++ tests/CleanTest.php
@@ -19,9 +19,14 @@ class CleanTest extends BaseStringSuite
             ['hello world.', '  hello world.'],
             ['Hello World. Foo Bar', '<p>Hello World.</p><!-- Comment --> <a href="#hash">Foo Bar</a>'],
             [
-                '<p>Hello World.</p> <a href="#hash">Foo Bar</a>',
+                '<p>Hello World.</p><a href="#hash">Foo Bar</a>',
                 '<p>Hello World.</p><!-- Comment --><a href="#hash">Foo Bar</a>',
                 '<p><a>'
+            ],
+            [
+                '<h1>Title</h1> <p>Foo Bar</p>',
+                '<h1>Title</h1> <p>Foo <a href="#hash">Bar</a></p>',
+                '<h1><p>'
             ],
         ];
     }
